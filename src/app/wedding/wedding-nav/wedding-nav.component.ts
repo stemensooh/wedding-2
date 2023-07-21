e7df383d3808b5af20dbe89e7910305bf0af5838
stemensooh <stemensooh@gmail.com>
2023-07-20 21:33:55 -0500

    initial commit

---
+++ src/app/wedding/wedding-nav/wedding-nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wedding-nav',
+  templateUrl: './wedding-nav.component.html',
+  styleUrls: ['./wedding-nav.component.scss']
+})
+export class WeddingNavComponent implements OnInit,OnDestroy {
+
+  constructor() { }
+
+  ngOnInit() { } 
+
+  ngOnDestroy(){
+  }
+}
